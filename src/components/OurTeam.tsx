a7961de15c39a97882cf530c7b54a68e887c22b2
Fernanda Medeiros <fecafumee@gmail.com>
2025-03-28 16:12:04 +0100

    removed estagiario card

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -16,27 +16,19 @@ const OurTeam = () => {
       imageUrl: '/images/profile1.jpg',
       initiallyFlipped: true, // This card will show the image side initially
     },
-    {
-      name: 'Fagner Camargo',
-      role: 'Estagiário',
-      imageUrl: '/images/profile2.jpg',
-    },
+    // {
+    //   name: 'Fagner Camargo',
+    //   role: 'Estagiário',
+    //   imageUrl: '/images/profile2.jpg',
+    // },
   ];
 
   return (
     <div className="py-[100px] md:py-[135px] flex flex-col gap-[65px]">
-      <h2 className="font-dm text-bgBlue text-3xl md:text-5xl text-center">
-        Conheça nossa equipe
-      </h2>
+      <h2 className="font-dm text-bgBlue text-3xl md:text-5xl text-center">Conheça nossa equipe</h2>
       <div className="flex flex-col items-center md:flex-row justify-center gap-6">
         {teamMembers.map((member, index) => (
-          <TeamCard
-            key={index}
-            name={member.name}
-            role={member.role}
-            imageUrl={member.imageUrl}
-            initiallyFlipped={member.initiallyFlipped}
-          />
+          <TeamCard key={index} name={member.name} role={member.role} imageUrl={member.imageUrl} initiallyFlipped={member.initiallyFlipped} />
         ))}
       </div>
     </div>
